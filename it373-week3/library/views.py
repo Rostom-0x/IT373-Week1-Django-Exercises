2b9b25a9eedacc329ab53901713d8e63e67dcc4e
Rostom-0x <rostombalboa05@gmail.com>
2025-10-13 18:21:28 +0800

    Add files via upload

---
+++ it373-week3/library/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from django.shortcuts import render
+from .models import Book, Category
+
+def home(request):
+    books = Book.objects.all()
+    categories = Category.objects.all()
+    return render(request, "library/home.html", {"books": books, "categories": categories})
+
